f773e8de8c01f24fc062f7e4dced7688c77922c0
TechQuery <shiy2008@gmail.com>
2022-12-09 04:08:00 +0800

    [add] pageSize & pageIndex input controls

--- source/Pager.tsx
+++ source/Pager.tsx
@@ -1,36 +1,77 @@
 import { ListModel } from 'mobx-restful';
 import { FC } from 'react';
-import { Pagination } from 'react-bootstrap';
+import { Pagination, Form } from 'react-bootstrap';
 
-export interface PagerProps
-  extends Pick<ListModel<{}>, 'pageIndex' | 'pageCount'> {
-  onChange?: (index: number) => any;
+export type PageMeta = Pick<ListModel<{}>, 'pageSize' | 'pageIndex'>;
+
+export interface PagerProps extends PageMeta {
+  pageCount: number;
+  onChange?: (meta: PageMeta) => any;
 }
 
-export const Pager: FC<PagerProps> = ({ pageIndex, pageCount, onChange }) => (
-  <Pagination className="my-0">
-    {pageIndex > 1 && (
-      <Pagination.Item onClick={() => onChange?.(1)}>1</Pagination.Item>
-    )}
-    {pageIndex > 3 && <Pagination.Ellipsis />}
-    {pageIndex > 2 && (
-      <Pagination.Item onClick={() => onChange?.(pageIndex - 1)}>
-        {pageIndex - 1}
-      </Pagination.Item>
-    )}
-    <Pagination.Item active>{pageIndex}</Pagination.Item>
-    {pageCount - pageIndex > 1 && (
-      <Pagination.Item onClick={() => onChange?.(pageIndex + 1)}>
-        {pageIndex + 1}
-      </Pagination.Item>
-    )}
-    {pageCount - pageIndex > 2 && <Pagination.Ellipsis />}
-    {pageIndex < pageCount && (
-      <Pagination.Item onClick={() => onChange?.(pageCount)}>
-        {pageCount}
-      </Pagination.Item>
-    )}
-  </Pagination>
+export const Pager: FC<PagerProps> = ({
+  pageSize,
+  pageIndex,
+  pageCount,
+  onChange,
+}) => (
+  <div className="d-flex align-items-center gap-3">
+    <Form.Control
+      type="number"
+      name="pageSize"
+      defaultValue={pageSize}
+      min={1}
+      required
+      onChange={({ currentTarget: input }) =>
+        input.reportValidity() &&
+        onChange?.({ pageSize: +input.value, pageIndex })
+      }
+    />
+    x
+    <Form.Control
+      type="number"
+      name="pageIndex"
+      defaultValue={pageIndex || 1}
+      min={1}
+      max={pageCount}
+      required
+      onChange={({ currentTarget: input }) =>
+        input.reportValidity() &&
+        onChange?.({ pageSize, pageIndex: +input.value })
+      }
+    />
+    <Pagination className="my-0">
+      {pageIndex > 1 && (
+        <Pagination.Item onClick={() => onChange?.({ pageSize, pageIndex: 1 })}>
+          1
+        </Pagination.Item>
+      )}
+      {pageIndex > 3 && <Pagination.Ellipsis />}
+      {pageIndex > 2 && (
+        <Pagination.Item
+          onClick={() => onChange?.({ pageSize, pageIndex: pageIndex - 1 })}
+        >
+          {pageIndex - 1}
+        </Pagination.Item>
+      )}
+      <Pagination.Item active>{pageIndex}</Pagination.Item>
+      {pageCount - pageIndex > 1 && (
+        <Pagination.Item
+          onClick={() => onChange?.({ pageSize, pageIndex: pageIndex + 1 })}
+        >
+          {pageIndex + 1}
+        </Pagination.Item>
+      )}
+      {pageCount - pageIndex > 2 && <Pagination.Ellipsis />}
+      {pageIndex < pageCount && (
+        <Pagination.Item
+          onClick={() => onChange?.({ pageSize, pageIndex: pageCount })}
+        >
+          {pageCount}
+        </Pagination.Item>
+      )}
+    </Pagination>
+  </div>
 );
 
 Pager.displayName = 'Pager';
