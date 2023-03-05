ce6b090c27dfee73179a4cf83fa7d026cbb8418a
TechQuery <shiy2008@gmail.com>
2023-03-06 01:51:49 +0800

    [optimize] Pager component supports SSR [optimize] REST Table component supports Column ignore [fix] "translator" typo

--- source/Pager.tsx
+++ source/Pager.tsx
@@ -1,6 +1,7 @@
 import { ListModel } from 'mobx-restful';
 import { FC } from 'react';
 import { Pagination, Form } from 'react-bootstrap';
+import { buildURLData, parseURLData } from 'web-utility';
 
 export type PageMeta = Pick<ListModel<{}>, 'pageSize' | 'pageIndex'>;
 
@@ -14,64 +15,66 @@ export const Pager: FC<PagerProps> = ({
   pageIndex,
   pageCount,
   onChange,
-}) => (
-  <div className="d-flex align-items-center gap-3">
-    <Form.Control
-      type="number"
-      name="pageSize"
-      defaultValue={pageSize}
-      min={1}
-      required
-      onChange={({ currentTarget: input }) =>
-        input.reportValidity() &&
-        onChange?.({ pageSize: +input.value, pageIndex })
-      }
-    />
-    x
-    <Form.Control
-      type="number"
-      name="pageIndex"
-      defaultValue={pageIndex || 1}
-      min={1}
-      max={pageCount}
-      required
-      onChange={({ currentTarget: input }) =>
-        input.reportValidity() &&
-        onChange?.({ pageSize, pageIndex: +input.value })
-      }
-    />
-    <Pagination className="my-0">
-      {pageIndex > 1 && (
-        <Pagination.Item onClick={() => onChange?.({ pageSize, pageIndex: 1 })}>
-          1
-        </Pagination.Item>
-      )}
-      {pageIndex > 3 && <Pagination.Ellipsis />}
-      {pageIndex > 2 && (
-        <Pagination.Item
-          onClick={() => onChange?.({ pageSize, pageIndex: pageIndex - 1 })}
-        >
-          {pageIndex - 1}
-        </Pagination.Item>
-      )}
-      <Pagination.Item active>{pageIndex}</Pagination.Item>
-      {pageCount - pageIndex > 1 && (
-        <Pagination.Item
-          onClick={() => onChange?.({ pageSize, pageIndex: pageIndex + 1 })}
-        >
-          {pageIndex + 1}
-        </Pagination.Item>
-      )}
-      {pageCount - pageIndex > 2 && <Pagination.Ellipsis />}
-      {pageIndex < pageCount && (
-        <Pagination.Item
-          onClick={() => onChange?.({ pageSize, pageIndex: pageCount })}
-        >
-          {pageCount}
-        </Pagination.Item>
-      )}
-    </Pagination>
-  </div>
-);
+}) => {
+  function propsOf(pageIndex = 1) {
+    const pagination = { pageSize, pageIndex };
+
+    return {
+      href: `?${buildURLData({ ...parseURLData(), ...pagination })}`,
+      onClick: () => onChange?.(pagination),
+    };
+  }
+
+  return (
+    <form
+      className="d-flex align-items-center gap-3"
+      onSubmit={onChange && (event => event.preventDefault())}
+    >
+      <Form.Control
+        type="number"
+        name="pageSize"
+        defaultValue={pageSize}
+        min={1}
+        required
+        onChange={({ currentTarget: input }) =>
+          input.reportValidity() &&
+          onChange?.({ pageSize: +input.value, pageIndex })
+        }
+      />
+      x
+      <Form.Control
+        type="number"
+        name="pageIndex"
+        defaultValue={pageIndex || 1}
+        min={1}
+        max={pageCount}
+        required
+        onChange={({ currentTarget: input }) =>
+          input.reportValidity() &&
+          onChange?.({ pageSize, pageIndex: +input.value })
+        }
+      />
+      <Pagination className="my-0">
+        {pageIndex > 1 && <Pagination.Item {...propsOf(1)}>1</Pagination.Item>}
+        {pageIndex > 3 && <Pagination.Ellipsis />}
+        {pageIndex > 2 && (
+          <Pagination.Item {...propsOf(pageIndex - 1)}>
+            {pageIndex - 1}
+          </Pagination.Item>
+        )}
+        <Pagination.Item active>{pageIndex}</Pagination.Item>
+        {pageCount - pageIndex > 1 && (
+          <Pagination.Item {...propsOf(pageIndex + 1)}>
+            {pageIndex + 1}
+          </Pagination.Item>
+        )}
+        {pageCount - pageIndex > 2 && <Pagination.Ellipsis />}
+        {pageIndex < pageCount && (
+          <Pagination.Item {...propsOf(pageCount)}>{pageCount}</Pagination.Item>
+        )}
+      </Pagination>
+    </form>
+  );
+};
 
 Pager.displayName = 'Pager';
