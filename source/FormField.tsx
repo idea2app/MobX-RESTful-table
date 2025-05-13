829d1f28c34b5be31b11a617f541fac3e78b5376
TechQuery <shiy2008@gmail.com>
2025-05-14 03:23:39 +0800

    [add] Select options in Form Field component [fix] File Field Label is missing in REST Form [optimize] upgrade Upstream packages

--- source/FormField.tsx
+++ source/FormField.tsx
@@ -1,20 +1,43 @@
-import { FC, InputHTMLAttributes } from 'react';
-import { FloatingLabelProps, Form, FormControlProps } from 'react-bootstrap';
+import { FC } from 'react';
+import { FloatingLabelProps, Form, FormControlProps, FormSelectProps } from 'react-bootstrap';
 
-export type FormFieldProps = InputHTMLAttributes<HTMLInputElement> &
-  FormControlProps &
-  Pick<FloatingLabelProps, 'label'>;
+export interface SelectOption
+  extends Partial<Pick<HTMLOptionElement, 'text' | 'label' | 'disabled'>> {
+  value: string;
+}
+
+export type FormFieldProps = FormControlProps &
+  FormSelectProps &
+  Pick<FloatingLabelProps, 'label'> & {
+    options?: SelectOption[];
+  };
 
 export const FormField: FC<FormFieldProps> = ({
   className,
   style,
   label,
+  placeholder,
   id,
   name,
+  options,
   ...controlProps
 }) => (
   <Form.FloatingLabel {...{ className, style, label }} controlId={name || id}>
-    <Form.Control name={name} placeholder={name || id} {...controlProps} />
+    {options ? (
+      <Form.Select name={name} {...controlProps}>
+        {options.map(({ value, text, label, disabled }) => (
+          <option key={value} {...{ value, label, disabled }}>
+            {text || value}
+          </option>
+        ))}
+      </Form.Select>
+    ) : (
+      <Form.Control
+        name={name}
+        placeholder={placeholder || (typeof label === 'string' ? label : name || id)}
+        {...controlProps}
+      />
+    )}
   </Form.FloatingLabel>
 );
 
